064b4df8ea292fdb00f3f31b3a7899e4ac136e77
Brandon Thomas Suit <brandon.suit@gmail.com>
2010-06-27 07:50:40 -0400

    Aaron Swartz' blog

--- mapper.py
+++ mapper.py
@@ -1,6 +1,40 @@
 from urlparse import urlparse
 import sys
 
+def get_scraper(content, uri):
+	"""Map the URI to the appropriate scraper class."""
+	dom = simplify_uri(uri)
+	mod = None
+
+	print "simplified uri: %s" %dom
+
+	# Major sites
+	if dom in ['slashdot.org', 'linux.slashdot.org']:
+		mod = 'slashdot'
+	elif dom == 'reddit.com':
+		mod = 'reddit'
+
+	# Personal blogs
+	mapp = {
+		'scobleizer.com': 'scobleizer',
+		'blog.broadbandmechanics.com': 'marcsvoice',
+		'aaronsw.com': 'aaronswartz',
+	}
+
+	if dom in mapp:
+		mod = mapp[dom]
+
+	print "module: %s" %mod
+
+	if not mod:
+		return False
+
+	mod = 'sites.' + mod
+
+	module = __import__(mod)
+	module = sys.modules[mod]
+	return module.get_scraper(content)
+
 def simplify_uri(uri):
 	"""Removes 'www', etc."""
 	up = urlparse(uri)
@@ -15,35 +49,4 @@ def simplify_uri(uri):
 		return uri
 
 	return domain[1:]
-
-def get_scraper(content, uri):
-	"""Map the URI to the appropriate scraper class."""
-	dom = simplify_uri(uri)
-	mod = None
-
-	print "simplified uri: %s" %dom
-
-	# Major sites
-	if dom in ['slashdot.org', 'linux.slashdot.org']:
-		mod = 'slashdot'
-	elif dom == 'reddit.com':
-		mod = 'reddit'
-
-	# Major blogs
-	elif dom == 'scobleizer.com':
-		mod = 'scobleizer'
-
-	elif dom == 'blog.broadbandmechanics.com':
-		mod = 'marcsvoice'
-
-	print "module: %s" %mod
-
-	if not mod:
-		return False
-
-	mod = 'sites.' + mod
-
-	module = __import__(mod)
-	module = sys.modules[mod]
-	return module.get_scraper(content)
 
---
+++ sites/aaronswartz.py
@@ -0,0 +1,58 @@
+from web2feed import Scraper, fix_uri, parse_date
+
+import re
+from datetime import datetime
+
+class AaronSwartzBlogScraper(Scraper):
+	"""Scraper for Aaron Swartz blog."""
+
+	def _extract_feed(self):
+		container = self.soup.find('td', attrs={'class':'content'})
+
+		# Aaron's blog requires event-parsing
+		stories = []
+		curStory = None
+		for tag in container:
+			try:
+				if not tag.name:
+					continue
+			except:
+				continue
+
+			if tag.name == 'h2':
+				if curStory and curStory['title'] and curStory['uri']:
+					stories.append(curStory)
+				curStory = {
+					'title': None,
+					'uri': None,
+					'date': None,
+					'contents': '',
+					'author': 'Aaron Swartz', # TODO openid
+				}
+				try:
+					a = tag.find('a', rel='bookmark')
+					curStory['title'] = a.text
+					curStory['uri'] = 'http://www.aaronsw.com/weblog/%s' % a.attrMap['href']
+
+				except:
+					pass
+
+				continue
+
+			try:
+				if tag['class'] == 'posted':
+					date = tag.a.text
+					date = re.sub('\s+', ' ', date).strip()
+					curStory['date'] = datetime.strptime(date, '%B %d, %Y')
+					continue
+			except:
+				pass
+
+			if tag:
+				curStory['contents'] += unicode(tag)
+
+		return stories
+
+def get_scraper(content):
+	return AaronSwartzBlogScraper(content)
+
